51f1a28004fbb1144221b3587a158f931f5d7c26
Javier Guerrero Huete <javigh87@gmail.com>
2020-10-21 08:45:04 +0100

    Introduciendo reto 03, encriptado de contraseñas

---
+++ M01 - Programación Vintage/14 - Reto 03 (M00 Ej 15).py
@@ -0,0 +1,38 @@
+import bcrypt
+"""
+--- Encriptando contraseñas ---
+
+Hacer el mismo programa que el ejercicio 15 del módulo 0 utilizando un diccionario y encriptando la contraseña con bcrypt
+"""
+
+usuarios = {
+        "pigmonchu":"lolailo1970",
+        "genaro23":"1234abc$",
+        "arrumako":"killo.2018"
+        }
+
+sal = bcrypt.gensalt() # sal?
+
+strUser = input("User....: ")
+strPwd = input("Password: ")
+
+bytePwd = strPwd.encode() # Contraseña en bytes, encriptada
+pwd_hashed = bcrypt.hashpw(bytePwd, sal) # Contraseña con sal(encriptada)
+
+no_encontrado = False
+
+for usuario in usuarios:
+    if strUser == usuario:
+
+        bytePwd1 = usuarios[strUser].encode()
+        pwd1_hashed = bcrypt.hashpw(bytePwd1, sal)
+
+        if bcrypt.checkpw(bytePwd, pwd1_hashed):
+
+            print("Ok, las contraseñas coinciden")
+            no_encontrado = True
+            break
+
+if not no_encontrado:
+    print("Usuario y/o contraseña incorrecto")
+
